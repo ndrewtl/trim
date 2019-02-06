d0ec0f89cbb6aa5bdcf2c73442e6df2afe0f669b
Andrew Lee <andrewl@mbda.fun>
2019-02-06 14:08:32 -0800

    move functions into Trim.CLI

---
+++ src/Trim/CLI.hs
@@ -0,0 +1,58 @@
+module Trim.CLI (Options, options, trim, readInput, writeOutput, run) where
+
+import System.IO
+import System.Directory
+import Options.Applicative
+import Trim
+
+data Options = Options
+  { newlines :: Bool
+  , output   :: Maybe FilePath
+  , input    :: FilePath }
+
+options :: Parser Options
+options =  Options
+  <$> switch
+    (  long  "newlines"
+    <> short 'N'
+    <> help  "If enabled, trim trailing newlines" )
+  <*> optional (strOption
+      (  long  "output"
+      <> short 'o'
+      <> help  "File to direct output" ))
+  <*> argument str
+    (  metavar "FILE"
+    <> value "-" )
+
+-- This method trims the given list of strings according to the options given
+trim :: [String] -> Options -> [String]
+trim inputLines opts =
+  let trimmed = map trimSpaces inputLines
+  in if newlines opts then trimLines trimmed else trimmed
+
+-- This method
+readInput :: Options -> IO String
+readInput opts =
+  let filename = input opts
+  in if filename == "-"
+    then getContents
+    else readFile filename
+
+-- This method writes the given string according to options
+writeOutput :: String -> Options -> IO ()
+writeOutput str opts =
+  case output opts of
+    Nothing      -> putStr str -- If no file is specified, then just print to stdout
+    Just outdest -> do -- Else, write to a temp file, then move it to the destination
+      (tname, thandle) <- openTempFile "." "trim"
+      hPutStr thandle str
+      hClose thandle
+      renameFile tname outdest
+
+-- This method is analogous to the 'main' method of most programs.
+-- Given options as input, output an IO action
+run :: Options -> IO ()
+run opts = do
+  inputLines <- fmap lines (readInput opts)
+  let output = unlines $ trim inputLines opts
+    in writeOutput output opts
