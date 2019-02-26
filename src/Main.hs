a5253fae7fa3af006d5c892f95a9be0190339d2c
Andrew Lee <andrewl@mbda.fun>
2019-02-26 10:57:39 -0800

    complete version option

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,12 @@ module Main where
 import Options.Applicative
 import Trim.CLI
 import Trim.CLI.Parser
+import qualified Paths_trim as Meta (version)
+import Data.Version (showVersion)
 
 main :: IO ()
 main =  do
   opts <- getOpts
   if version opts
-    then putStrLn "version string"
+    then putStrLn ("trim " ++ showVersion Meta.version)
     else run opts
